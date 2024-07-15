9c53816860b7c08c08f82acb5b0a72bd2161761a
Greg  Careme <gregor.ten1@icloud.com>
2024-07-15 11:29:25 +0200

    Run deploy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,12 +6,23 @@ function Footer() {
       <h1 className="footer__h1">Image Buddy</h1>
 
       <div className="footer__links">
-        <img
-          className="footer__linkedin"
-          src="/images/LinkedIn.png"
-          alt="LinkedIn"
-        />
-        <img className="footer__github" src="/images/GitHub.png" alt="GitHub" />
+        <a href="https://es.linkedin.com/in/gregoire-careme-77269a265">
+          <img
+            className="footer__linkedin"
+            src="/images/LinkedIn.png"
+            alt="LinkedIn"
+          />
+        </a>
+        <a
+          href=" https://github.com/GreggCarem
+"
+        >
+          <img
+            className="footer__github"
+            src="/images/GitHub.png"
+            alt="GitHub"
+          />
+        </a>
       </div>
     </footer>
   );
